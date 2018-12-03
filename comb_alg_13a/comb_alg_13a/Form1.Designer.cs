c2abf2c5b01349ee2266ae901f4635d1932de9b3
Lena <alena4729@gmail.com>
2018-12-03 21:34:16 +0300

    initial commit

---
+++ comb_alg_13a/comb_alg_13a/Form1.Designer.cs
@@ -0,0 +1,234 @@
+﻿namespace comb_alg_13a
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.dgvInput = new System.Windows.Forms.DataGridView();
+            this.dgvRes = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbRes = new System.Windows.Forms.TextBox();
+            this.btnRes = new System.Windows.Forms.Button();
+            this.lblCols = new System.Windows.Forms.Label();
+            this.lblRows = new System.Windows.Forms.Label();
+            this.tbRows = new System.Windows.Forms.TextBox();
+            this.tbCols = new System.Windows.Forms.TextBox();
+            this.btnCreate = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvInput)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvRes)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.splitContainer1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 78);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(662, 246);
+            this.panel1.TabIndex = 10;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.tbRes);
+            this.panel2.Controls.Add(this.btnRes);
+            this.panel2.Controls.Add(this.lblCols);
+            this.panel2.Controls.Add(this.lblRows);
+            this.panel2.Controls.Add(this.tbRows);
+            this.panel2.Controls.Add(this.tbCols);
+            this.panel2.Controls.Add(this.btnCreate);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(662, 78);
+            this.panel2.TabIndex = 11;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.dgvInput);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.dgvRes);
+            this.splitContainer1.Size = new System.Drawing.Size(662, 246);
+            this.splitContainer1.SplitterDistance = 318;
+            this.splitContainer1.TabIndex = 1;
+            // 
+            // dgvInput
+            // 
+            this.dgvInput.AllowUserToAddRows = false;
+            this.dgvInput.AllowUserToDeleteRows = false;
+            this.dgvInput.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvInput.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvInput.Location = new System.Drawing.Point(0, 0);
+            this.dgvInput.Name = "dgvInput";
+            this.dgvInput.Size = new System.Drawing.Size(318, 246);
+            this.dgvInput.TabIndex = 1;
+            this.dgvInput.CellValueChanged += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvInput_CellValueChanged);
+            // 
+            // dgvRes
+            // 
+            this.dgvRes.AllowUserToAddRows = false;
+            this.dgvRes.AllowUserToDeleteRows = false;
+            this.dgvRes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvRes.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvRes.Location = new System.Drawing.Point(0, 0);
+            this.dgvRes.Name = "dgvRes";
+            this.dgvRes.ReadOnly = true;
+            this.dgvRes.Size = new System.Drawing.Size(340, 246);
+            this.dgvRes.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(530, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(120, 13);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "Максимальная сумма";
+            // 
+            // tbRes
+            // 
+            this.tbRes.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbRes.Location = new System.Drawing.Point(533, 37);
+            this.tbRes.Name = "tbRes";
+            this.tbRes.ReadOnly = true;
+            this.tbRes.Size = new System.Drawing.Size(117, 20);
+            this.tbRes.TabIndex = 16;
+            // 
+            // btnRes
+            // 
+            this.btnRes.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.btnRes.Location = new System.Drawing.Point(269, 34);
+            this.btnRes.Name = "btnRes";
+            this.btnRes.Size = new System.Drawing.Size(75, 23);
+            this.btnRes.TabIndex = 15;
+            this.btnRes.Text = "Результат";
+            this.btnRes.UseVisualStyleBackColor = true;
+            this.btnRes.Click += new System.EventHandler(this.btnRes_Click);
+            // 
+            // lblCols
+            // 
+            this.lblCols.AutoSize = true;
+            this.lblCols.Location = new System.Drawing.Point(91, 21);
+            this.lblCols.Name = "lblCols";
+            this.lblCols.Size = new System.Drawing.Size(91, 13);
+            this.lblCols.TabIndex = 10;
+            this.lblCols.Text = "Кол-во столбцов";
+            // 
+            // lblRows
+            // 
+            this.lblRows.AutoSize = true;
+            this.lblRows.Location = new System.Drawing.Point(12, 21);
+            this.lblRows.Name = "lblRows";
+            this.lblRows.Size = new System.Drawing.Size(73, 13);
+            this.lblRows.TabIndex = 11;
+            this.lblRows.Text = "Кол-во строк";
+            // 
+            // tbRows
+            // 
+            this.tbRows.Location = new System.Drawing.Point(15, 37);
+            this.tbRows.Name = "tbRows";
+            this.tbRows.Size = new System.Drawing.Size(60, 20);
+            this.tbRows.TabIndex = 12;
+            this.tbRows.TextChanged += new System.EventHandler(this.tbRows_TextChanged);
+            // 
+            // tbCols
+            // 
+            this.tbCols.Location = new System.Drawing.Point(94, 37);
+            this.tbCols.Name = "tbCols";
+            this.tbCols.Size = new System.Drawing.Size(60, 20);
+            this.tbCols.TabIndex = 13;
+            this.tbCols.TextChanged += new System.EventHandler(this.tbCols_TextChanged);
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.btnCreate.Location = new System.Drawing.Point(188, 34);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(75, 23);
+            this.btnCreate.TabIndex = 14;
+            this.btnCreate.Text = "Создать";
+            this.btnCreate.UseVisualStyleBackColor = true;
+            this.btnCreate.Click += new System.EventHandler(this.btnCreate_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(662, 324);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvInput)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvRes)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.DataGridView dgvInput;
+        private System.Windows.Forms.DataGridView dgvRes;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbRes;
+        private System.Windows.Forms.Button btnRes;
+        private System.Windows.Forms.Label lblCols;
+        private System.Windows.Forms.Label lblRows;
+        private System.Windows.Forms.TextBox tbRows;
+        private System.Windows.Forms.TextBox tbCols;
+        private System.Windows.Forms.Button btnCreate;
+    }
+}
+
---
+++ comb_alg_13a/comb_alg_13a/Form1.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Data;
+using System.Windows.Forms;
+
+namespace comb_alg_13a
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int n = 0, m = 0;
+        int[][] matrix;
+
+        private void tbRows_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                m = int.Parse(tbRows.Text);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Неверный формат ввода количества строк матрицы!");
+                tbRows.Focus();
+                tbRows.SelectAll();
+                return;
+            }
+            if (m < 1)
+            {
+                MessageBox.Show("Количество строк не может быть меньше 1!");
+                tbRows.Focus();
+                tbRows.SelectAll();
+            }
+        }
+
+        private void tbCols_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                n = int.Parse(tbCols.Text);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Неверный формат ввода количества столбцов матрицы!");
+                tbCols.Focus();
+                tbCols.SelectAll();
+                return;
+            }
+            if (n < 1) 
+            {
+                MessageBox.Show("Количество столбцов не корректно!");
+                tbCols.Focus();
+                tbCols.SelectAll();
+            }
+        }
+
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
+            if (tbRows.Text == "")
+            {
+                MessageBox.Show("Количество строк не корректно!");
+                tbRows.Focus();
+                tbRows.SelectAll();
+                return;
+            }
+            if (tbCols.Text == "")
+            {
+                MessageBox.Show("Количество столбцов не корректно!");
+                tbCols.Focus();
+                tbCols.SelectAll();
+                return;
+            }           
+            CreateGrid(dgvInput, n, m);
+        }
+
+        private void CreateGrid(DataGridView dgv, int cnt_cols, int cnt_rows)
+        {
+            DataTable matr = new DataTable("matr");
+            DataColumn[] cols = new DataColumn[cnt_cols];
+
+            for (int i = 0; i < cnt_cols; i++)
+            {
+                cols[i] = new DataColumn(i.ToString());
+                matr.Columns.Add(cols[i]);
+            }
+
+            for (int i = 0; i < cnt_rows; i++)
+            {
+                DataRow newRow;
+                newRow = matr.NewRow();
+                matr.Rows.Add(newRow);
+            }
+
+            dgv.DataSource = matr;
+            dgv.AutoResizeColumns();
+        }
+
+        private int[][] GridToMatrix(DataGridView dgv, int cnt_cols, int cnt_rows)
+        {
+            int[][] matr = new int [cnt_rows][];
+            for (int i = 0; i < cnt_rows; i++)
+                matr[i] = new int[cnt_cols];
+
+            DataGridViewCell txtCell;
+            for (int i = 0; i < cnt_rows; i++)
+            {
+                for (int j = 0; j < cnt_cols; j++)
+                {
+                    txtCell = dgv.Rows[i].Cells[j];
+                    string s = txtCell.Value.ToString();
+                    if (s == "") //незапомлненные ячейки заполняем нулями
+                        matr[i][j] = 0;
+                    else
+                        matr[i][j] = Int32.Parse(s);
+                }
+            }
+            return matr;
+        }
+
+        private void MatrixToGrid(DataGridView dgv, int[][] matr, int cnt_cols, int cnt_rows)
+        {
+            for (int i = 0; i < cnt_rows; i++)
+            {
+                for (int j = 0; j < cnt_cols; j++)
+                {
+                    dgv.Rows[i].Cells[j].Value = matr[i][j].ToString();
+                }
+            }
+        }
+
+        private void dgvInput_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        {
+            int row = e.RowIndex,
+                col = e.ColumnIndex;
+            try
+            {
+                string str = dgvInput.Rows[row].Cells[col].Value.ToString();
+                int value = Int32.Parse(str);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Некорректно задано значение ячейки "+"("+row+","+col+").\nБудет заменено на 0.");
+                dgvInput.Rows[row].Cells[col].Value = 0;
+               // dgvInput.CurrentCell = dgvInput.Rows[row].Cells[col];
+               // dgvInput.CurrentCell.Selected = true;
+               // dgvInput.BeginEdit(true);
+                return;
+            }
+
+        }
+
+        private void btnRes_Click(object sender, EventArgs e)
+        {
+            matrix = GridToMatrix(dgvInput, n, m); //наша исходная матрица
+                                                   
+            
+        }
+    }
+}
